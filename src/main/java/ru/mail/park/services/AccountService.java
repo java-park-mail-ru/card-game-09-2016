63ba7bcaae4df1692ec76fd6e839d9b76c681478
devil1001 <jenek-devil@mail.ru>
2016-10-04 23:33:23 +0300

    add to deathtest

--- src/main/java/ru/mail/park/services/AccountService.java
+++ src/main/java/ru/mail/park/services/AccountService.java
@@ -8,17 +8,34 @@ import ru.mail.park.model.UserProfile;
 import java.util.HashMap;
 import java.util.Map;
 
+
 @Service
 public class AccountService {
     private Map<String, UserProfile> userNameToUser = new HashMap<>();
-
 
     public UserProfile addUser(String login, String password, String email) {
         final UserProfile userProfile = new UserProfile(login, email, password);
         userNameToUser.put(login, userProfile);
         return userProfile;
     }
+
     public UserProfile getUser(String login) {
         return userNameToUser.get(login);
     }
+
+    public void removeUser(String login) {
+        if(userNameToUser.containsKey(login)) {
+            userNameToUser.remove(login);
+        }
+    }
+
+    public void changeUser(String oldLogin, String newLogin, String password, String email) {
+        if (userNameToUser.containsKey(oldLogin)) {
+            UserProfile temp = getUser(oldLogin);
+            temp.setEmail(email);
+            temp.setLogin(newLogin);
+            temp.setPassword(password);
+        }
+    }
+
 }
