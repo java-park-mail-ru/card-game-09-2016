9133096c8d70ae1576aa5b1d3e896309b8e4d67b
alexandr <smoke_91@list.ru>
2016-11-17 20:03:45 +0300

    add test

--- src/main/java/ru/mail/park/Application.java
+++ src/main/java/ru/mail/park/Application.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -27,4 +29,10 @@ public class Application {
             }
         };
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
 }
--- src/main/java/ru/mail/park/services/AccountService.java
+++ src/main/java/ru/mail/park/services/AccountService.java
@@ -1,35 +1,32 @@
 package ru.mail.park.services;
 
 
-import org.springframework.context.annotation.Bean;
+import org.jetbrains.annotations.Nullable;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.mail.park.model.UserProfile;
 import java.sql.PreparedStatement;
 import java.sql.Statement;
-import java.util.Objects;
 
-
+@Transactional
 @Service
 public class AccountService {
     private final JdbcTemplate template;
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
-    
-    public AccountService(JdbcTemplate template) {
-        this.template = template;
+    private final PasswordEncoder passwordEncoder;
 
+    public AccountService(JdbcTemplate template, PasswordEncoder passwordEncoder) {
+        this.template = template;
+        this.passwordEncoder = passwordEncoder;
     }
     
+    @Nullable
     public UserProfile addUser(String login, String password, String email) {
         try {
 			KeyHolder keyHolder = new GeneratedKeyHolder();
@@ -37,10 +34,9 @@ public class AccountService {
                         PreparedStatement pst = cnctn.prepareStatement(
                                 "INSERT INTO `Users` (`email`,`login`,`password`) VALUES (?,?,?)",
 								Statement.RETURN_GENERATED_KEYS);
-                        int index = 0;
-                        pst.setString(++index, email);
-                        pst.setString(++index, login);
-                        pst.setString(++index, passwordEncoder().encode(password));
+                        pst.setString(1, email);
+                        pst.setString(2, login);
+                        pst.setString(3, passwordEncoder.encode(password));
                         return pst;
                     }
 					, keyHolder);
@@ -50,9 +46,10 @@ public class AccountService {
 		}
     }
 
+    @Nullable
     public UserProfile getUser(Integer id) {
         try {
-            String sql = "SELECT `id`,`login`, `score` FROM `Users` WHERE `login` = ?;";
+            final String sql = "SELECT `id`,`login`, `score` FROM `Users` WHERE `login` = ?;";
             return template.queryForObject(sql, userProfileRowMapper, id);
         } catch (EmptyResultDataAccessException na) {
             return null;
@@ -61,9 +58,9 @@ public class AccountService {
 
     public Integer getId(String login, String password) {
         try {
-            String sql = "SELECT `id`, `password` FROM `Users` WHERE `login` = ? ;";
-            Security security = template.queryForObject(sql, securityRowMapper,login);
-            if (!passwordEncoder().matches(password, security.getPassword())){
+            final String sql = "SELECT `id`, `password` FROM `Users` WHERE `login` = ? ;";
+            final Security security = template.queryForObject(sql, securityRowMapper,login);
+            if (!passwordEncoder.matches(password, security.getPassword())){
                 return -1;
             }
             return security.getId();
@@ -72,20 +69,14 @@ public class AccountService {
         }
     }
     public int removeUser(String login) {
-        try {
-            String sql = "DELETE  FROM `Users` WHERE `login` = ?;";
-            return template.update(sql, login);
-        } catch (EmptyResultDataAccessException na){
-            return 0;
-        }
+        final String sql = "DELETE  FROM `Users` WHERE `login` = ?;";
+        return template.update(sql, login);
     }
 
     public int changeUser(String oldLogin, String newLogin, String password, String email) {
         try {
-            String sql = "UPDATE `Users` SET `login` = ?, `email` = ?, `password` = ?  WHERE `login` = ?;";
-            return template.update(sql,newLogin,email, password, oldLogin);
-        } catch (EmptyResultDataAccessException na){
-            return 0;
+            final String sql = "UPDATE `Users` SET `login` = ?, `email` = ?, `password` = ?  WHERE `login` = ?;";
+            return template.update(sql,newLogin,email, passwordEncoder.encode(password), oldLogin);
         }catch (DuplicateKeyException dk) {
             return -1;
         }
