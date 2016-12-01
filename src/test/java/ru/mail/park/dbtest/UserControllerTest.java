b2a6bdf49c4737d0f9a241261219e8762a1c3abf
alexandr <smoke_91@list.ru>
2016-12-01 13:25:29 +0300

    работа с тестами

--- src/test/java/ru/mail/park/dbtest/UserControllerTest.java
+++ src/test/java/ru/mail/park/dbtest/UserControllerTest.java
@@ -1,5 +1,7 @@
 package ru.mail.park.dbtest;
 
+import org.eclipse.jetty.http.HttpStatus;
+import org.eclipse.jetty.server.Response;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,16 +9,18 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.autoconfigure.web.servlet.MockMvcPrint;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@AutoConfigureMockMvc(print = MockMvcPrint.NONE)
+@AutoConfigureMockMvc(/*print = MockMvcPrint.NONE*/)
 @Transactional
 public class UserControllerTest {
 
@@ -27,17 +31,17 @@ public class UserControllerTest {
     public void testCreate() throws Exception {
         final String user = "{\"login\": \"user1\", \"email\": \"example@mail.ru\" \"password\": \"GOD\" }";
 
-        mockMvc.perform(post("/api/use")
+        mockMvc.perform(post("/api/user")
                 .content(user)
                 .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("code").value("200"))
+                .andExpect(status().isOk())
                /* .andExpect(jsonPath("response.sessionid").isString())
                 .andExpect(jsonPath("response.id").isNumber())*/;
 
-        mockMvc.perform(post("/api/session")
-                .content(user)
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("code").value("200"))
+      //  mockMvc.perform(post("/api/session")
+      //          .content(user)
+      //          .contentType(MediaType.APPLICATION_JSON))
+      //          .andExpect(jsonPath("code").value("200"))
               /*  .andExpect(jsonPath("response.sessionid").isNumber())
                 .andExpect(jsonPath("response.id").isNumber())*/;
 
