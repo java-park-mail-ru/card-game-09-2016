e9b552f35458b73086379d561c0ca6c4a032f004
alexandr <smoke_91@list.ru>
2016-12-08 09:31:41 +0300

    Контроллер юзеров покрыли

--- src/test/java/ru/mail/park/dbtest/UserControllerTest.java
+++ src/test/java/ru/mail/park/dbtest/UserControllerTest.java
@@ -5,18 +5,18 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
-import ru.mail.park.model.User.UserAuto;
 import ru.mail.park.model.User.UserCreate;
 import ru.mail.park.model.User.UserProfile;
 import ru.mail.park.main.MainController;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Random;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -42,6 +42,31 @@ public class UserControllerTest {
         int countUser = 150;
         userCreateHashMap(countUser);
         userDefault();
+    }
+
+    @Test
+    public void testUserTop() throws Exception{
+        int countUser = 150;
+        int maxLimit = 25;
+        int maxRequest = 10;
+        Random random = new Random();
+        userCreateHashMap(countUser);
+        List<UserProfile> users;
+        for (int i=0,limit,since; i<maxRequest; i++){
+            limit = random.nextInt(maxLimit);
+            since = random.nextInt(countUser);
+            users = MainController.getAccountService().getTop(limit, since);
+            ResultActions result = mockMvc.perform(get("/api/top")
+                    .param("limit", String.valueOf(limit))
+                    .param("since_id", String.valueOf(since)));
+                result.andExpect(jsonPath("code").value(0));
+            for (int j =0 ; j<users.size(); j++){
+                result.andExpect(jsonPath("response.["+j+"].id").value(users.get(j).getId()))
+                      .andExpect(jsonPath("response.["+j+"].login").value(users.get(j).getLogin()))
+                      .andExpect(jsonPath("response.["+j+"].score").value(users.get(j).getScore()));
+            }
+        }
+
     }
 
     public HashMap<String,UserCreate> userCreateHashMap(int cause) throws Exception{
